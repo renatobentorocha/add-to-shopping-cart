9f83fd59f677d346d4cc3221e655930ebfd4b5e6
Renato Rocha <renatob.rocha@gmail.com>
2021-01-30 18:16:26 -0300

    Init config, add shopping cart svg

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ShoppingCart from './src/components/svg/ShoppingCart';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ShoppingCart width={150} height={150}/>
     </View>
   );
 }
